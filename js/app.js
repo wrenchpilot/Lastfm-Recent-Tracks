a1df853deedd25684c41a364a22962ae5838bb10
James Shawn Carnley <Shawn.Carnley@gatech.edu>
2019-01-05 14:55:02 -0500

    Updated to update track on an interval

--- js/app.js
+++ js/app.js
@@ -1,12 +1,3 @@
-/*---------------
- * jQuery Last.Fm Plugin by Pinceladas da Web
- * Example and documentation at: https://github.com/pinceladasdaweb/Lastfm-Recent-Tracks
- * Copyright (c) 2013
- * Version: 1.0.0 (13-ABR-2013)
- * Licensed under the MIT license: http://www.opensource.org/licenses/mit-license.php
- * Requires: jQuery v1.9 or later and Handlebars 1.0 or later
----------------*/
-
 var Lastfm = {
 	init: function(config) {
 		this.url = 'http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user='+config.username+'&api_key='+config.apikey+'&limit='+config.count+'&format=json';
@@ -14,14 +5,15 @@ var Lastfm = {
 		this.container = config.container;
 		this.fetch();
 	},
+	
 	attachTemplate: function() {
 		var template = Handlebars.compile(this.template);
 
 		this.container.empty().append(template(this.tracks));
 	},
+	
 	fetch: function() {
 		var self = this;
-
 		$.getJSON(this.url, function(data) {
 			var feed = data.recenttracks.track;
 
@@ -32,17 +24,20 @@ var Lastfm = {
 					artist: track.artist['#text'],
 					album: track.album['#text'],
 					link: track.url
-				}
+				};
 			});
 			self.attachTemplate();
 		});
 	}
+};
+function getTrack(){
+	 Lastfm.init({
+		template: $('#tracks-template').html(),
+		container: $('.container'),
+		username: '<USER NAME>',
+		count: "1",
+		apikey: '<YOUR API KEY>'
+	});
 }
-
-Lastfm.init({
-	template: $('#tracks-template').html(),
-	container: $('.container'),
-	username: 'rj',
-	count: 18,
-	apikey: '<API KEY HERE>'
-})
+getTrack();
+var interval = setInterval(getTrack, 30000);
